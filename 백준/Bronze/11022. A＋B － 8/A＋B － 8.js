1431b7bb17da345f090e5748b31d12ee76da6389
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-01 16:21:12 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,16 @@
+// fs 모듈의, readFileSync 함수를 이용해서 동기적으로 해당 경로의 파일 전체를 읽어들인다.
+// 백준에서는 '/dev/stdin' 경로에 테스트 케이스 파일이 있다.
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+// 읽어들인 정보는, toString() 함수를 통해, 문자열로 변환하여야 사용할 수 있다.
+// 그리고, 입력 받은 문자열을 split() 함수를 통해 배열화한다. \n : 줄바꿈
+
+for (let i = 1; i <= input[0]; i++) {
+    // split 메서드를 이용해 A와 B를 공백 기준으로 분리 해주고,
+    let numbers = input[i].split(' ').map(Number);
+    
+    let num1 = numbers[0];
+    let num2 = numbers[1];
+    
+    // 숫자 앞에 $를 붙여줘서 출력
+    console.log(`Case #${i}: ${num1} + ${num2} = ${num1 + num2}`);
+}
