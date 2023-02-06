9545f7696226bd24797c2207d12b8ef60b267144
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-06 10:49:59 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    var answer = 0;  // 변수 생성
+    
+    // 변수에, 정수1과 정수2를 나눈 몫을 저장한다.
+    answer = Math.floor(num1 / num2)  
+    return answer;  // 그 값을 담은 변수를 반환
+}
