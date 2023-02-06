8ad7c968ecf02cdcc15c80a2f3994fe426c16bc8
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-06 14:36:33 +0900

    Delete 프로그래머스/lv0/120843. 공 던지기 directory

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++
@@ -1,8 +0,0 @@
-function solution(numbers, k) {
-  const ballNext = current => (current + 2) % numbers.length;
-  let current = 0;
-    
-  for (let i = 0; i < k - 1; i++) current = ballNext(current);
-    
-  return numbers[current];
-}
