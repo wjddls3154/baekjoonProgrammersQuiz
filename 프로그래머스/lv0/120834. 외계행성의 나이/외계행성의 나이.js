a24ac920106d92e4d603e4d4a93c93545ab4718d
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-06 10:45:06 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++
@@ -1,8 +0,0 @@
-function solution(age) {
-    // a~j 까지, 하나의 문자열에 담는다.
-    let str = 'abcdefghij'; 
-    // 문자 결과 값에 +를 붙이면 숫자로 변하는 것을 이용.
-    return Array.from(age.toString())
-        .map(x => str[+x])
-        .join('');
-}
--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++
@@ -1,4 +0,0 @@
-function solution(emergency) {
-    let sequence = emergency.slice().sort((a,b) => b-a);
-    return emergency.map( x => sequence.indexOf(x)+1);
-}
