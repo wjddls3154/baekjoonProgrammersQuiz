f9b6aaf9a27b9d63d196ca29a91ccb877fcbf98a
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-11 17:29:37 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,11 @@
+// ◎
+function solution(quiz) {
+    var answer = [];
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
+}
