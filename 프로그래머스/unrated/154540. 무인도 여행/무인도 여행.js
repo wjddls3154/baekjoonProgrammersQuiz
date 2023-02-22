85e035d23c89c67d4b0a5a9717df27c4196a740f
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-02-22 13:49:49 +0900

    [unrated] Title: 무인도 여행, Time: 0.34 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
@@ -0,0 +1,61 @@
+// ◎
+function solution(maps) {
+    
+  // 배열 안 문자열을, 배열 안의 각각 문자로 변환해준다.  Ex) ["X591X","X1X5...] -> [["X", "5", "9" ...] ...]
+  const newMap = maps.map((n) => n.split(""));
+
+  // 상 하 좌 우 좌표를 미리 셋팅해둔다.
+  const dx = [1, 0, -1, 0];
+  const dy = [0, 1, 0, -1];
+
+  // DFS 
+  function DFS(x, y, num) {
+    
+    let sum = Number(num);  // num은 현재 String이므로, 숫자로 변환해준다.
+
+    // 설정해둔 좌표 배열을 통해, 상 하 좌 우를 탐색한다.
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      // 지도를 벗어나지 않는지 판단한다.
+      if (nx >= 0 && ny >= 0 && nx < newMap.length && ny < newMap[0].length) {
+        
+        // 지도를 벗어나지 않고, "X"(바다)가 아닌 곳을 찾는다면
+        if (newMap[nx][ny] !== "X") {
+          
+          const next = newMap[nx][ny];  // 식량 수를 저장하고
+          newMap[nx][ny] = "X";  // 그 곳을 "X"로 변환하고
+
+          // DFS를 통해서, 그 좌표부터 다시 탐색을 한다. 그리고 그 리턴 값을 sum에 더해준다.
+          sum += DFS(nx, ny, next);
+        }
+      }
+    }
+
+    return sum;  // 마지막으로, 다 더해진 sum을 리턴한다.
+  }
+
+    
+  const result = [];
+  // 이중 반복문을 통해서, 하나씩 살펴본다.
+  for (let i = 0; i < newMap.length; i++) {
+    for (let j = 0; j < newMap[0].length; j++) {
+      
+      // "X"가 아닌 곳을 찾게된다면
+      if (newMap[i][j] !== "X") {
+
+        
+        const start = newMap[i][j];  // 일단 현재 좌표를 저장해둔다.
+        newMap[i][j] = "X";  // 그 후 그 곳을 X로 변환한다.
+
+        // DFS(x좌표, y좌표, 식량 수)를 실행하고, 그에 대한 반환 값을 result에 추가(push)한다.
+        result.push(DFS(i, j, start));
+      }
+    }
+  }
+
+  // 삼항 연산자 이용 : 만약, result의 길이가 0이라면 ? [-1]을 반환하고,
+  // 아니라면 result 배열을, 오름차순으로 정렬 후 반환해준다.
+  return result.length ? result.sort((a, b) => a - b) : [-1];
+}
