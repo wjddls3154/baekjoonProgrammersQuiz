68dd66fe3c64ff262e27603a4a6478fe5ef60a03
정인교 <37132897+wjddls3154@users.noreply.github.com>
2023-03-04 23:59:29 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,12 @@
+function solution(array, n) {
+  return array.filter(a => a === n).length;
+}
+
+/*
+* 해결법 *
+filter 함수는 특정 조건에 해당되는 것만 추려낼 때 쓰는 함수이다.
+
+Array.prototype.filter ( callbackfn [ , thisArg ] )
+- filter()는 배열 내 각 요소에 대해 한 번 제공된 callback 함수를 호출해, callback이 true로 강제하는 값을 반환하는 모든 값이 있는 새로운 배열을 생성한다.
+- filter() 는 일치하는 결과 모두를 배열로 반환한다. find()는 하나의 값만 반환한다.
+*/
